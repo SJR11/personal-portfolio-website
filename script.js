6c49cf48aacb08cd3984bd1587333bf09ee2eb1f
Srijan Khadka <srijankhadkajr11@gmail.com>
2025-07-10 19:04:41 +0545

    added project udaan

--- script.js
+++ script.js
@@ -64,3 +64,14 @@ seeLessBtn.addEventListener('click', () => {
       });
     });
   });
+
+  const modal = document.getElementById('pdfModal');
+  const btn = document.getElementById('openModal');
+  const span = document.querySelector('.close');
+
+  btn.onclick = () => modal.style.display = 'block';
+  span.onclick = () => modal.style.display = 'none';
+  window.onclick = (e) => {
+    if (e.target == modal) modal.style.display = 'none';
+  };
+
